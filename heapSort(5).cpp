2c362f34b6a4f2c9bfcfff1b7c65c174f033cafb
in1t-s3lf <36043458+in1t-s3lf@users.noreply.github.com>
2021-07-04 14:29:13 +0300

    Create heapSort(5).cpp

---
+++ heapSort(5).cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+#include <vector>
+
+#include <string>
+
+using namespace std;
+vector < int > heap;
+
+void siftUp(int v) {
+  if (v == 0) return;
+  int p = (v - 1) / 2;
+  if (heap[v] < heap[p]) {
+    swap(heap[v], heap[p]);
+    siftUp(p);
+  }
+}
+void siftDown(int v) {
+  int left = 2 * v + 1, right = 2 * v + 2;
+  if (left >= heap.size()) return;
+  if (left == heap.size() - 1) right = left;
+  int imin = heap[left] < heap[right] ? left : right;
+  if (heap[v] > heap[imin]) {
+    swap(heap[v], heap[imin]);
+    siftDown(imin);
+  }
+}
+int main() {
+  string str;
+  while (cin >> str) {
+    if (str == "ADD") {
+      int x = 0;
+      cin >> x;
+      heap.push_back(x);
+      siftUp(heap.size() - 1);
+    } else if (str == "EXTRACT") {
+      if (!heap.empty()) {
+        cout << heap[0] << '\n';
+        heap[0] = heap.back();
+        heap.pop_back();
+        siftDown(0);
+      } else {
+        cout << "CANNOT\n";
+      }
+    } else if (str == "CLEAR") {
+      heap = vector < int > ();
+    }
+  }
+  return 0;
+
+}
